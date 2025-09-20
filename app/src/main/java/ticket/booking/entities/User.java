1f0475ec86e0cb4f0a870ab6cb52e43b7916a369
Ashutosh Kumar <ak2417942@gmail.com>
2025-09-21 01:25:55 +0530

    constructor , getter and setter added for User entity

--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -10,5 +10,36 @@ public class User {
     private String hashedPassword;
     private List<Ticket> ticketsBooked;
 
+    public User(String name, String address, String email, String uuid, String hashedPassword, List<Ticket> ticketsBooked) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.uuid = uuid;
+        this.hashedPassword = hashedPassword;
+        this.ticketsBooked = ticketsBooked;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public List<Ticket> getTicketsBooked() {
+        return ticketsBooked;
+    }
 }
--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -2,12 +2,12 @@ package ticket.booking.services;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.corba.se.spi.ior.ObjectKey;
 import ticket.booking.entities.User;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class UserBookingSerice {
 
@@ -18,9 +18,20 @@ public class UserBookingSerice {
 
     private static final String USERS_PATH = "./LocalDB/User.json";
 
-    public UserBookingSerice(User loggedUser) throws IOException {
-        this.user = loggedUser;
+    public UserBookingSerice(User newUser) throws IOException {
+        this.user = newUser;
         File users = new File(USERS_PATH);
+        /* We are using object mapper for de-serializing the json data.
+        * And vice - versa of this is called serialization
+        */
         this.userList = OBJECT_MAPPER.readValue(users, new TypeReference<List<User>>() {});
+    }
+
+    public Boolean loginUser(User newUser){
+        Optional<User> foundUser = userList.stream().filter( user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getHashedPassword(), user1)
+        }).findFirst();
+
+        return foundUser.isPresent();
     }
 }
