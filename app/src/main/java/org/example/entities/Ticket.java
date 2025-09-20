ba6720e4d96dfa22f01a53acf24a88aaf6a3e712
Ashutosh Kumar <ak2417942@gmail.com>
2025-09-20 23:19:35 +0530

    Entitites created User , Ticket and Train

---
+++ app/src/main/java/org/example/entities/Ticket.java
@@ -0,0 +1,11 @@
+package org.example.entities;
+
+import java.util.Date;
+
+public class Ticket {
+    private String ticketId;
+    private String usedId;
+    private Date dateOfTravel;
+    private Train userTrain;
+
+}
---
+++ app/src/main/java/org/example/entities/User.java
@@ -0,0 +1,12 @@
+package org.example.entities;
+
+public class User {
+    private String name;
+    private String address;
+    private String email;
+    private String uuid;  /* Unique user id for the user */
+    private String hashedPassword;
+    private List<Ticket> ticketsBooked;
+
+
+}
